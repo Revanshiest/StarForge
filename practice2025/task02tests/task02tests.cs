eb73f5503b062c02ffd565136770cc87cfd71a7d
Revanshiest <emezhenov@gmail.com>
2025-06-27 21:51:52 +0600

    test: correction by xUnit format

--- practice2025/task02tests/task02tests.cs
+++ practice2025/task02tests/task02tests.cs
@@ -1,15 +1,16 @@
 namespace task02tests;
 
-using #task02
-using Xunit
-
+using task02;
+using Xunit;
+using System.Collections.Generic;
+using System.Linq;
 
 public class StudentServiceTests
 {
     private List<Student> _testStudents;
     private StudentService _service;
 
-    public void StudentServiceTests()
+    public StudentServiceTests()
     {
         _testStudents = new List<Student>
         {
@@ -24,15 +25,15 @@ public class StudentServiceTests
     public void GetStudentsByFaculty_ReturnsCorrectStudents()
     {
         var result = _service.GetStudentsByFaculty("ФИТ").ToList();
-        Assert.AreEqual(2, result.Count);
-        Assert.IsTrue(result.All(s => s.Faculty == "ФИТ"));
+        Assert.Equal(2, result.Count);
+        Assert.True(result.All(s => s.Faculty == "ФИТ"));
     }
 
     [Fact]
     public void GetFacultyWithHighestAverageGrade_ReturnsCorrectFaculty()
     {
         var result = _service.GetFacultyWithHighestAverageGrade();
-        Assert.AreEqual("Экономика", result);
+        Assert.Equal("Экономика", result);
     }
 
     [Fact]
@@ -46,19 +47,19 @@ public class StudentServiceTests
     }
 
     [Fact]
-    public void GetStudentsOrderedByName_ReturnsStudentsSortedAlphabetically
+    public void GetStudentsOrderedByName_ReturnsStudentsSortedAlphabetically()
     {
         var result = _service.GetStudentsOrderedByName().ToList();
 
-        Assert.Equal('Анна', result[0].Name)
-        Assert.Equal('Иван', result[1].Name)
-        Assert.Equal('Пётр', result[2].Name)
+        Assert.Equal("Анна", result[0].Name);
+        Assert.Equal("Иван", result[1].Name);
+        Assert.Equal("Петр", result[2].Name);
     }
 
     [Fact]
     public void GroupStudentsByFaculty_ReturnsCorrectGroups()
     {
-        var result= _service.GroupStudentsByFaculty()
+        var result= _service.GroupStudentsByFaculty();
 
         var NamesFit = new[] { "Иван", "Анна" };
         var NamesEcon = new[] { "Петр" };
@@ -66,19 +67,11 @@ public class StudentServiceTests
         Assert.True(
             result.Contains("ФИТ") &&
             result["ФИТ"].All(s => NamesFit.Contains(s.Name))
-        )
+        );
 
         Assert.True(
             result.Contains("Экономика") &&
             result["Экономика"].All(s => NamesEcon.Contains(s.Name))
-        )
-    }
-
-    [Fact]
-    public void GetFacultyWithHighestAverageGrade_ReturnsCorrectFaculty()
-    {
-        var result = _service.GetFacultyWithHighestAverageGrade
-
-        Assert.Equal("Экономика", result)
+        );
     }
 }
