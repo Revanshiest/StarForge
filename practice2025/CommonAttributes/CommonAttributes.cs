3c8d0d86710dd44fc607b4e7a6b527134e389826
Revanshiest <emezhenov@gmail.com>
2025-07-12 04:44:40 +0600

    feat: add CommonAttributes

---
+++ practice2025/CommonAttributes/CommonAttributes.cs
@@ -0,0 +1,17 @@
+﻿namespace CommonAttributes;
+
+using System;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+public sealed class DisplayNameAttribute : Attribute
+{
+    public string Name { get; }
+    public DisplayNameAttribute(string name) => Name = name;
+}
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+public sealed class VersionAttribute : Attribute
+{
+    public string Version { get; }
+    public VersionAttribute(string version) => Version = version;
+}
